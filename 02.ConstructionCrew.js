cf72a1d8a7ea4f7fa9d4d205109bf976e637b003
Alex <golearnweb@gmail.com>
2017-07-21 21:43:50 +0300

    Initial commit

---
+++ 02.ConstructionCrew.js
@@ -0,0 +1,15 @@
+function modifyWorker(worker) {
+    if (worker.handsShaking == true) {
+        worker.bloodAlcoholLevel += worker.experience * worker.weight * 0.1;
+        worker.handsShaking = false;
+    }
+    return worker;
+}
+
+console.log(modifyWorker({
+        weight: 80,
+        experience: 1,
+        bloodAlcoholLevel: 0,
+        handsShaking: true
+    }
+));
